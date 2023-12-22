672750bbd38f6ef19b07b16fbc634831931fa673
wen <741912512@qq.com>
2023-12-22 20:37:46 +0800

    第一次提交

---
+++ src/constants/index.ts
@@ -0,0 +1,6 @@
+export const SYSTEM_LOGO = "https://img2.baidu.com/it/u=1773700895,2445751674&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=500";
+
+/**
+ * 语雀公共库地址
+ */
+export const PLANET_LINK = "https://www.yuque.com/hongupup/mkyiq8/nwpkrb1vnr7hpfbi?singleDoc# 《注意注意！》";
---
+++ src/image/index.ts
@@ -0,0 +1,6 @@
+// src/image/index.ts
+
+// 导入并导出文件夹中的内容
+export * from './background_login.png';
+export * from './logo_login.jpg';
+// 其他导出...
